274e5a65bfdbfd4d0378011ca7bea6ec769d07cc
Victor Lazaro <lazarofilm@gmail.com>
2020-09-20 19:15:29 -0400

    Updates

--- precache-manifest.f94143b571a52c159e563c9e1d84dbfe.js
+++ precache-manifest.f94143b571a52c159e563c9e1d84dbfe.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c7ba476a0f411dabd386f4fbd3e36f05",
+    "revision": "fa486ada1d3a6294acc8a59deb5bb147",
     "url": "/markdown-previewer/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/markdown-previewer/static/css/2.d9ad5f5c.chunk.css"
   },
   {
-    "revision": "0806b76946c235422ecd",
+    "revision": "5c1850046b862ccbbe2f",
     "url": "/markdown-previewer/static/css/main.2a3311c9.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/markdown-previewer/static/js/2.44fc33cd.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "0806b76946c235422ecd",
-    "url": "/markdown-previewer/static/js/main.d511ad09.chunk.js"
+    "revision": "5c1850046b862ccbbe2f",
+    "url": "/markdown-previewer/static/js/main.31903ecf.chunk.js"
   },
   {
     "revision": "380a9cf99d31db8da525",
