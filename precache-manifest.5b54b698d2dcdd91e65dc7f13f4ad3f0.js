471fae9cdb3300de0c8ad1fc10837d5e6e1bea11
Victor Lazaro <lazarofilm@gmail.com>
2020-09-20 15:40:52 -0400

    Updates

---
+++ precache-manifest.5b54b698d2dcdd91e65dc7f13f4ad3f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7ba476a0f411dabd386f4fbd3e36f05",
+    "url": "/markdown-previewer/index.html"
+  },
+  {
+    "revision": "5b5f780e7ec5211a910b",
+    "url": "/markdown-previewer/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "0806b76946c235422ecd",
+    "url": "/markdown-previewer/static/css/main.2a3311c9.chunk.css"
+  },
+  {
+    "revision": "5b5f780e7ec5211a910b",
+    "url": "/markdown-previewer/static/js/2.44fc33cd.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/markdown-previewer/static/js/2.44fc33cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0806b76946c235422ecd",
+    "url": "/markdown-previewer/static/js/main.d511ad09.chunk.js"
+  },
+  {
+    "revision": "380a9cf99d31db8da525",
+    "url": "/markdown-previewer/static/js/runtime-main.4fd0b226.js"
+  }
+]);
