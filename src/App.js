7ababb335f857aac5b09cf509f92eb7aa6720865
Victor Lazaro <lazarofilm@gmail.com>
2020-09-21 15:58:09 -0400

    Added Markdown CSS Stylesheet

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
-import "./App.scss";
+import "./github-markdown.css";
 // import { Button } from "react-bootstrap";
 
 class App extends Component {
@@ -8,7 +8,7 @@ class App extends Component {
     value:
       "# Welcome to my React Markdown Previewer!\n\n## This is a sub-heading...\n### And here's some other cool stuff:\n  \nHeres some code, `<div></div>`, between 2 backticks.\n\n```\n// this is multi-line code:\n\nfunction anotherExample(firstLine, lastLine) {\n  if (firstLine == '```' && lastLine == '```') {\n    return multiLineCode;\n  }\n}\n```\n  \nYou can also make text **bold**... whoa!\nOr _italic_.\nOr... wait for it... **_both!_**\nAnd feel free to go crazy ~~crossing stuff out~~.\n\nThere's also [links](https://www.freecodecamp.com), and\n> Block Quotes!\n\nAnd if you want to get really crazy, even tables:\n\nWild Header | Crazy Header | Another Header?\n------------ | ------------- | ------------- \nYour content can | be here, and it | can be here....\nAnd here. | Okay. | I think we get it.\n\n- And of course there are lists.\n  - Some are bulleted.\n     - With different indentation levels.\n        - That look like this.\n\n\n1. And there are numbererd lists too.\n1. Use just 1s if you want! \n1. And last but not least, let's not forget embedded images:\n\n![React Logo w/ Text](https://techchronos.com/wp-content/uploads/SszarkLabs/stack-icon/cywBkaGwkMeDAuJbSt1k.png)\n",
     markdowned: (
-      <div id="preview" class="Preview">
+      <div id="preview" className="Preview">
         <p>Markdowned goes here</p>
       </div>
     ),
@@ -51,15 +51,16 @@ class App extends Component {
     return (
       <div className="App">
         <div className="MarkdownMachine">
-          <table>
-            <tr>
-              <th>Type things here:</th>
-              <th>And see them rendered here:</th>
-            </tr>
-            <tr>
-              <th>
-                {/* prettier-ignore */}
-                <textarea
+          <table className="two-tables">
+            <tbody>
+              <tr>
+                <th>Type things here:</th>
+                <th>And see them rendered here:</th>
+              </tr>
+              <tr>
+                <th className="two-columns">
+                  {/* prettier-ignore */}
+                  <textarea
                 className="TextArea"
                 name="editor"
                 id="editor"
@@ -69,9 +70,10 @@ class App extends Component {
                 onChange={this.handleChange}
                 onLoad={this.handleChange}
                />
-              </th>
-              <th>{this.state.markdowned}</th>
-            </tr>
+                </th>
+                <th>{this.state.markdowned}</th>
+              </tr>
+            </tbody>
           </table>
         </div>
       </div>
