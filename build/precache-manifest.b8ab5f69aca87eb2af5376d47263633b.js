b3f3f8302e5ab18a4b72ae777051e0c61cdade28
Victor Lazaro <lazarofilm@gmail.com>
2020-09-21 16:01:43 -0400

    forgot to deploy...

--- build/precache-manifest.b8ab5f69aca87eb2af5376d47263633b.js
+++ build/precache-manifest.b8ab5f69aca87eb2af5376d47263633b.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1a07b560644cc1a61f7e98692ed06de0",
+    "revision": "fd1d3ff2364a3444a21fe29996b15520",
     "url": "/markdown-previewer/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/markdown-previewer/static/css/2.d9ad5f5c.chunk.css"
   },
   {
-    "revision": "85e30a2d340ac028c3e4",
-    "url": "/markdown-previewer/static/css/main.cb41d810.chunk.css"
+    "revision": "22eb9caac30b36d1b14f",
+    "url": "/markdown-previewer/static/css/main.165c36b7.chunk.css"
   },
   {
     "revision": "5b5f780e7ec5211a910b",
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/markdown-previewer/static/js/2.44fc33cd.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "85e30a2d340ac028c3e4",
-    "url": "/markdown-previewer/static/js/main.b3b60e89.chunk.js"
+    "revision": "22eb9caac30b36d1b14f",
+    "url": "/markdown-previewer/static/js/main.018fbdb7.chunk.js"
   },
   {
     "revision": "380a9cf99d31db8da525",
